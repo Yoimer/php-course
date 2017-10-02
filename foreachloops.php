21f0a3323dfc585daf10fa2dc6d3f346987c74de
Yoimer <ydrf19@gmail.com>
2017-10-02 16:54:12 -0400

    Added foreachloops.php

---
+++ foreachloops.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+  <head>
+    <title>Loops: foreachloops</title>
+  </head>
+  <body>
+		<!-- no incrementing as in for or while loops
+		    it just knows it has to move to the bottom of the array
+		    there is no condition either -->
+		<?php
+			$ages = array(4, 8, 15, 16, 23, 42);
+			foreach ($ages as $age) {
+				echo "Age: {$age}<br />";
+			}
+		?>
+		
+		<br />
+		
+		<?php // foreach using assoc.array
+			$person = array (
+						"first_name" => "Kevin",
+						"last_name"  => "Skoglund",
+						"address"    => "123 Main Street",
+						"city"       => "Beverly Hills",
+						"state"      => "CA",
+						"zip_code"   => "90210",
+			);
+			
+			foreach($person as $attribute => $data) {
+				$attr_nice = ucwords (str_replace("_", " ", $attribute));
+				echo "{$attr_nice}: {$data}<br />";
+			}
+		?>
+		
+		<br />
+		
+		<?php
+			$prices = array("Brand New Computer" => 2000,
+							"1 month of Lynda.com" => 25,
+							"Learning PHP" => null);
+							
+							
+			foreach ($prices as $item => $price) {
+				echo "{$item}: ";
+				if (is_int($price)) {
+					echo "$" . $price;
+				}
+				else {
+					echo "priceless";
+				}
+				echo "<br />";
+			}
+		?>
+		
+  </body>
+</html>
+
